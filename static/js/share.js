e0030ecfdb7d07c87a38b43d05dc5b9ee2efb5cd
Vitaliy Levitski <vitaliylevitskiand@gmail.com>
2016-12-02 00:13:44 +0200

    Add vk sharing

--- static/js/share.js
+++ static/js/share.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    document.getElementById('vk_share_button').innerHTML = VK.Share.button(false, {
+    document.getElementById('vk_share_button').innerHTML = VK.Share.button({
+        url: 'http://ansiart.online/ru/',
         type: "custom",
         text: "<img src=\"https://vk.com/images/share_32.png\" width=\"32\" height=\"32\" />"
     })
