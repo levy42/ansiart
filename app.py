b6abfffef24bf47ed4ded0cff6a846772a53b8ce
Vitaliy Levtiski <groghkov@maprtech.com>
2016-12-10 21:38:24 +0200

    tmp

--- app.py
+++ app.py
@@ -1,46 +1,11 @@
-import json
-from flask import Flask
-from flask import render_template, request
-from PIL import Image
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="./")
 
-SIZES = [('M', 70), ('XS', 30), ('S', 50), ('L', 90), ('XL', 120)]
-DEFAULT = ['  ', '. ', '..', '.-', '--', '-+', '++', '**', 'HH', 'H#', '##']
-PALETTES = {'Default': DEFAULT}
-PALETTES.update(json.loads(file("palette.json").read()))
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        f = request.files.get('file')
-        size = int(request.form['size'])
-        inverse = True if request.form.get('inverse') else False
-        palette = PALETTES[request.form['palette']]
-        try:
-            im = Image.open(f)
-            if not inverse:
-                palette = list(reversed(palette))
-                palette.append(palette[-1])
-            shadow_step = 255 / (len(palette) - 1)
-            w_h = float(im.height) / im.width
-            im = im.resize((size, int(size * w_h)))
-            im = im.convert(mode="L")
-            text = ""
-            for i in range(im.height):
-                for j in range(im.width):
-                    p = im.getpixel((j, i))
-                    text += palette[p / shadow_step]
-                text += "\n"
-        except Exception as e:
-            error = "Failed to create ANSI picture. Check your image file"
-            return render_template("index.html", sizes=SIZES,
-                                   palettes=PALETTES, error=error)
-        return render_template("index.html", image=text, inverse=inverse,
-                               sizes=SIZES, palettes=PALETTES)
-    else:
-        return render_template("index.html", sizes=SIZES, palettes=PALETTES)
+    return render_template("index.html")
 
 
 app.run(port=4000, host='0.0.0.0')
